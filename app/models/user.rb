472c904f1e28e9b0e52ff6fd85f2fdb8ec55d478
MitchScho <scho.mitch@gmail.com>
2022-06-08 22:00:59 +0000

    fix bugs related to empty cart and aurthorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 4 }
   validates :password_confirmation, presence: true, length: { minimum: 4 }
